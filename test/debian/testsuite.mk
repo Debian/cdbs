4c0bdaaed2fb2c731a50d4a267e26f6ccc1e3209
petere <petere@19660600-52fe-0310-9875-adc0d7a7b53c>
2006-06-06 21:21:56 +0000

    Replaced most uses of := by = in makefiles.  This generally gives more reasonable behavior.
    git-svn-id: svn+ssh://svn.debian.org/svn/build-common/trunk@18 19660600-52fe-0310-9875-adc0d7a7b53c

--- test/debian/testsuite.mk
+++ test/debian/testsuite.mk
@@ -15,8 +15,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 # 02111-1307 USA.
 # cdbs uses itself to build itself.
-_cdbs_package_root_dir := $(CURDIR)/../../..
-_cdbs_scripts_path := $(_cdbs_package_root_dir)/scripts
-_cdbs_rules_path := $(_cdbs_package_root_dir)/1/rules
-_cdbs_class_path := $(_cdbs_package_root_dir)/1/class
-_cdbs_makefile_suffix := .in
+_cdbs_package_root_dir = $(CURDIR)/../../..
+_cdbs_scripts_path = $(_cdbs_package_root_dir)/scripts
+_cdbs_rules_path = $(_cdbs_package_root_dir)/1/rules
+_cdbs_class_path = $(_cdbs_package_root_dir)/1/class
+_cdbs_makefile_suffix = .in
