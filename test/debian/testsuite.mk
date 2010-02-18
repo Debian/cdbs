40b312762f1fc1ad01a42ec5916f3cfbf7ec4331
Jonas Smedegaard <dr@jones.dk>
2010-02-19 00:41:06 +0100

    Refer to FSF website (not postal address) in headers.

--- test/debian/testsuite.mk
+++ test/debian/testsuite.mk
@@ -11,9 +11,8 @@
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-# 02111-1307 USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # cdbs uses itself to build itself.
 _cdbs_package_root_dir = $(CURDIR)/../../..
 _cdbs_scripts_path = $(_cdbs_package_root_dir)/scripts
