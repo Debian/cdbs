0e6c420ead481999fab998cb738e2ccf200ee1c1
Jonas Smedegaard <dr@jones.dk>
2016-05-11 14:20:01 +0200

    Relicense (where permitted) to GPL-3+.

--- test/autotools-3.sh
+++ test/autotools-3.sh
@@ -2,10 +2,10 @@
 # -*- mode: sh; coding: utf-8 -*-
 # Copyright © 2003 Jeff Bailey <jbailey@debian.org>
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2, or (at
-# your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
--- test/pbuilder-all.sh
+++ test/pbuilder-all.sh
@@ -3,10 +3,10 @@
 # Try to build all CDBS-using packages
 # Copyright © 2003 Colin Walters <walters@debian.org>
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2, or (at
-# your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
